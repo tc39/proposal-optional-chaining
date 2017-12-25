fbad9acee23bf08d62cc9dda5aa51f9f9b0b7084
Daniel Ehrenberg <littledan@chromium.org>
2017-12-25 21:26:21 +0100

    Travis auto-deployment

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ev
+
+# Enable SSH authentication
+
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+
+if [[ $ENCRYPTED_KEY == "" ]]; then
+    echo "Auto-deploy GitHub key missing; exiting without a build"
+    exit 0
+fi
+
+$(npm bin)/set-up-ssh --key "$ENCRYPTED_KEY" \
+                      --iv "$ENCRYPTED_IV" \
+                      --path-encrypted-key "$PRIVATE_KEY_FILE_NAME"
+
+# Update the content from the `gh-pages` branch
+
+$(npm bin)/update-branch --commands "mkdir out && ecmarkup spec.html out/index.html" \
+                         --commit-message "Update gh-pages [skip ci]" \
+                         --directory "out" \
+                         --distribution-branch "gh-pages" \
+                         --source-branch "master"
+
